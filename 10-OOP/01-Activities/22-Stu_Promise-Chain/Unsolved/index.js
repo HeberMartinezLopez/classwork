8c7655caa8d4c9be2c45a10e09f0e3af688e1e59
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-08-16 19:24:36 -0700

    update classwork files

--- 10-OOP/01-Activities/22-Stu_Promise-Chain/Unsolved/index.js
+++ 10-OOP/01-Activities/22-Stu_Promise-Chain/Unsolved/index.js
@@ -7,11 +7,20 @@ const BlogPost = require('./lib/blogPost');
 readFile('./data/post.json', 'utf-8').then((json) => {
   console.log('Received data from post.json');
   // TODO: parse the json string and assign the resulting object to a variable
-  //
+  const blogData = JSON.parse(json);
+
   // TODO: Use the BlogPost class to create a new BlogPost object and use its `render()` method to return an html string.
-  //
+  const post = new BlogPost(
+    post.title,
+    post.text,
+    post.author,
+    post.createdOn
+    );
+  const html = post.render();
   // TODO: Write the html to file
-  //
+  return writeFile('./dist/post.html').then(() => {
+    console.log('Created post.html!');
+  })
 });
 
 // TODO: print a message in the console only after the html file is written.
