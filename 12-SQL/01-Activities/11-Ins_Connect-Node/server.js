89baeef5d4147401a1e36cbf22d4712489a05997
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-08-25 20:00:48 -0700

    update classwork

--- 12-SQL/01-Activities/11-Ins_Connect-Node/server.js
+++ 12-SQL/01-Activities/11-Ins_Connect-Node/server.js
@@ -16,14 +16,14 @@ const db = mysql.createConnection(
     // MySQL username,
     user: 'root',
     // MySQL password
-    password: '',
+    password: 'root',
     database: 'classlist_db'
   },
   console.log(`Connected to the classlist_db database.`)
 );
 
 // Query database
-db.query('SELECT * FROM students', function (err, results) {
+db.query('SELECT * FROM students', (err, results) => {
   console.log(results);
 });
 
