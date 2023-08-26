fbba6f2b2a55c870a590b60eef8a40fe356178ff
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-08-25 20:01:14 -0700

    update classwork

--- 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
+++ 12-SQL/01-Activities/04-Stu_Databases/Unsolved/schema.sql
@@ -1,1 +1,5 @@
 -- Write code here or enter directly in MySQL shell --
+DROP DATABASE sample_db;
+
+CREATE DATABASE books_db;
+USE books_db;
--- 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/06-Stu_Tables/Unsolved/db/schema.sql
@@ -2,5 +2,8 @@ DROP DATABASE IF EXISTS books_db;
 -- Creates the "books_db" database --
 CREATE DATABASE books_db;
 USE books_db;
-
 -- Add biography table --
+CREATE TABLE biographies(
+    id INT NOT NULL,
+    name VARCHAR(100) NOT NULL
+);
