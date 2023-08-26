bdf2ad1b8c8f24076fca909f2921f116f33830d9
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-08-25 21:51:30 -0700

    update classwork

--- 12-SQL/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,16 @@
 -- Write your schema here --
+DROP DATABASE IF EXISTS inventory_db;
+CREATE DATABASE inventory_db;
+USE inventory_db;
+
+CREATE TABLE products(
+    id INT PRIMARY KEY,
+    product_name VARCHAR(30) NOT NULL,
+    category_name VARCHAR(100) NOT NULL,
+    price INT NOT NULL,
+    in_stock BOOLEAN NOT NULL
+);
+CREATE TABLE categories(
+    id INT PRIMARY KEY,
+    category_name VARCHAR(30)
+);
--- 12-SQL/01-Activities/18-Stu_Seeds/Unsolved/db/schema.sql
+++ 12-SQL/01-Activities/18-Stu_Seeds/Unsolved/db/schema.sql
@@ -3,8 +3,9 @@ CREATE DATABASE grocery_db;
 
 USE grocery_db;
 
+DROP TABLE IF EXISTS products;
 CREATE TABLE products(
-  id INT NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   product_name VARCHAR(30) NOT NULL,
   category_name VARCHAR(30) NOT NULL
 );
