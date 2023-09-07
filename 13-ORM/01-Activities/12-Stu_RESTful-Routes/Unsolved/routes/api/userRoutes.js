c93103a7384c9f502d38ef10667616ea42905856
Heber Martinez Lopez <hebermartinez14@gmail.com>
2023-09-06 20:59:04 -0700

    update classwork files

--- 13-ORM/01-Activities/12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
+++ 13-ORM/01-Activities/12-Stu_RESTful-Routes/Unsolved/routes/api/userRoutes.js
@@ -6,13 +6,34 @@ const User = require('../../models/User');
 
 // GET a user
 router.get('/:id', async (req, res) => {
-  const userData = await User.findByPk(req.params.id).catch((err) =>
-    res.json(err)
-  );
-  res.json(userData);
+  try {
+    const userData = await User.findByPk(req.params.id);
+    if(!userData) {
+      return res.status(404).json({message: 'User not found :('})
+    }
+    res.status(200).json(userData);
+  } catch(err) {
+      res.status(500).json(err);
+    }
 });
 
 // UPDATE a user
+// router.put
+// try{
+//   await User.update(req.body, {
+//     where: {
+//       id: req.params.id,
+//     },
+//   });
+
+//   const userData = await User.findByPk(req.params.id);
+
+//   if(!userData){
+//     return res.status(404).json({message: 'User not found! :('});
+//   }
+// res.status(200).json(userData);
+// };
+
 router.put('/:id', async (req, res) => {
   const userData = await User.update(req.body, {
     where: {
